6afcdd0e7161502b7e6263ae3ecf42647ce5753b
mohitkumar <mohit@freightexchange.com.au>
2022-12-29 14:52:50 +0530

    fix restart grpc conn

--- src/main/java/io/github/mohitkumar/orchy/example/Main.java
+++ src/main/java/io/github/mohitkumar/orchy/example/Main.java
@@ -10,12 +10,15 @@ import java.util.function.Function;
 public class Main {
     public static void main(String[] args) {
         Function<Map<String, Object>, Map<String, Object>> addDataFn= (Map<String, Object> input) ->{
-            System.out.println(input);
             input.put("newKey",22);
             return input;
         };
         Function<Map<String, Object>, Map<String, Object>> printFn= (Map<String, Object> input) ->{
             System.out.println(input);
+            return input;
+        };
+        Function<Map<String, Object>, Map<String, Object>> enhanceDataFn= (Map<String, Object> input) ->{
+            input.put("key", "prefix_"+input.get("key"));
             return input;
         };
         Worker worker1 = Worker
@@ -24,9 +27,14 @@ public class Main {
         Worker worker2 = Worker
                 .newBuilder()
                 .DefaultWorker(printFn,"print-worker", 2,10,TimeUnit.MICROSECONDS).build();
+
+        Worker worker3 = Worker
+                .newBuilder()
+                .DefaultWorker(enhanceDataFn,"enhanceData", 2,10,TimeUnit.MICROSECONDS).build();
         WorkerManager manager = new WorkerManager("localhost",8099);
         manager.registerWorker(worker1,1);
         manager.registerWorker(worker2,1);
+        manager.registerWorker(worker3,1);
         manager.start();
     }
 }
